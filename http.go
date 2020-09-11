2ad0820f0c0b73a8a47bbf094a3c26d12e38277d
Nitigya Kapoor <kapoornitigya@gmail.com>
2020-09-12 05:02:39 +0530

    http server and clients

---
+++ http.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+    ctx := req.Context()
+    fmt.Println("server: hello handler started")
+    defer fmt.Println("server: hello handler ended")
+
+    select {
+    case <-time.After(10 * time.Second):
+        fmt.Fprintf(w, "hello\n")
+    case <-ctx.Done():
+
+        err := ctx.Err()
+        fmt.Println("server:", err)
+        internalError := http.StatusInternalServerError
+        http.Error(w, err.Error(), internalError)
+    }
+}
+
+func main() {
+
+    resp, err := http.Get("http://gobyexample.com")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("Response status:", resp.Status)
+
+    scanner := bufio.NewScanner(resp.Body)
+    for i := 0; scanner.Scan() && i < 5; i++ {
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        panic(err)
+	}
+	
+	http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", nil)
+	
+	http.HandleFunc("/hello", hello)
+    http.ListenAndServe(":8090", nil)
+}
